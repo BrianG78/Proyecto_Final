522d33f98ba3a83576de9d46ba97fe92e21a2282
BrianG78 <brayanhack28@gmail.com>
2023-11-15 13:34:27 -0600

    2do parcial

--- Tienda/Tienda/settings.py
+++ Tienda/Tienda/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Servidor de correo
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'brayanhack28@gmail.com'
+EMAIL_HOST_PASSWORD = 'jzrm yyub nydj apyg'
+
+#Configuracion opcional del correo
+DEFAULT_FROM_EMAIL = 'brayanhack28@gmail.com'
