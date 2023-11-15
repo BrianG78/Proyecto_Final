daa7ca2d671120c9b81e2c9660eeb3f2911376c7
BrianG78 <2019452041@teschi.edu.mx>
2023-11-15 17:50:08 -0600

    render4

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z%h9lng#c=v1j-s%f_lubc_skk$9gfydznfbm_h7(aa!-@0#22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
