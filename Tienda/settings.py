46f00fec18b02132cdf78d38949527bc8c86a1d8
BrianG78 <2019452041@teschi.edu.mx>
2023-11-15 18:19:04 -0600

    chapurender

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z%h9lng#c=v1j-s%f_lubc_skk$9gfydznfbm_h7(aa!-@0#22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
