ddefe3010d79e0a94027e1850bc95265b9f097d7
BrianG78 <2019452041@teschi.edu.mx>
2023-11-15 18:25:47 -0600

    renderlin

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z%h9lng#c=v1j-s%f_lubc_skk$9gfydznfbm_h7(aa!-@0#22'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 ALLOWED_HOSTS = []
 
+DEBUG = 'RENDER' not in os.environ
+
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = BASE_DIR / 'api/static'
+    
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
